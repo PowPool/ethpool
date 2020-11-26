3e6b993df98d045a0551c86be57cd363af037a57
mutalisk <0x08@0x08.net>
2020-11-26 09:52:31 +0800

    add debug log to check parameters of eth_submitHashrate

--- proxy/stratum.go
+++ proxy/stratum.go
@@ -145,6 +145,7 @@ func (cs *Session) handleTCPMessage(s *ProxyServer, req *StratumReq) error {
 	case "eth_submitHashrate":
 		return cs.sendTCPResult(req.Id, true)
 	default:
+		Debug.Println("eth_submitHashrate: %v", req.Params)
 		errReply := s.handleUnknownRPC(cs, req.Method)
 		return cs.sendTCPError(req.Id, errReply)
 	}
